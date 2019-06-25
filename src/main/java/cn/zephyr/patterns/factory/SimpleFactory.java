8b03a55d9dc807cb4ea2e513e94c35b2aa7c8a97
zephyrlai <laizonghao@foxmail.com>
2019-06-25 15:36:35 +0800

    first commit

---
+++ src/main/java/cn/zephyr/patterns/factory/SimpleFactory.java
@@ -0,0 +1,9 @@
+package cn.zephyr.patterns.factory;
+
+/**
+ * @Author: laizonghao
+ * @Description:
+ * @Date: 2019-06-25 15:29
+ */
+public class SimpleFactory {
+}
---
+++ src/test/java/cn/zephyr/factory/FactoryPatternTest.java
@@ -0,0 +1,15 @@
+package cn.zephyr.factory;
+
+import org.junit.Test;
+
+/**
+ * @Author: laizonghao
+ * @Description:
+ * @Date: 2019-06-25 15:28
+ */
+public class FactoryPatternTest {
+    @Test
+    public void test01(){
+        System.err.println("112233");
+    }
+}
